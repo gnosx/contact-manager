4dc2cc20c75819b54806ea035d095d78ab365123
Ke-TW82 <141370825+Ke-TW82@users.noreply.github.com>
2025-02-17 17:43:40 -0500

    Fix contactdatabase

--- contactdatabase.php
+++ contactdatabase.php
@@ -2,7 +2,7 @@
 $server = "34.205.154.206";
 $username = "group4";
 $password = "fouristhebest";
-$database = "contactMan";
+$database = "ContactMan";
 
 $conn = new mysqli($server, $username, $password, $database); 
 if ($conn->connect_error) {
