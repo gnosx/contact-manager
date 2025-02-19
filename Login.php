5f05e2ab9753f8220cb532166c9bccde02264f1d
gnosx <qexaura21@gmail.com>
2025-02-18 23:06:49 -0500

    Fixed SQL line

--- Login.php
+++ Login.php
@@ -12,14 +12,14 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
+		$stmt = $conn->prepare("SELECT ID, Login, Password FROM Users WHERE Login=? AND Password=?");
 		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+			returnWithInfo( $row['ID'] );
 		}
 		else
 		{
