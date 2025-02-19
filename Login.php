563772342a4c898a05d37af79cb2a16f5ccbbb13
gnosx <qexaura21@gmail.com>
2025-02-18 22:36:19 -0500

    Fixed SQL line

--- Login.php
+++ Login.php
@@ -12,7 +12,7 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=? AND Password =?");
+		$stmt = $conn->prepare("SELECT ID, FirstName, LastName FROM Users WHERE Login=? AND Password=?");
 		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
