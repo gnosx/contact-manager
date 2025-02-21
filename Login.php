472905b65fadb54db5c9169b7b4200de5bf537e6
jacksontshaw <148835425+jacksontshaw@users.noreply.github.com>
2025-02-21 13:47:55 -0500

    add session / store user data

--- Login.php
+++ Login.php
@@ -35,7 +35,7 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson( $obj ) 
 	{
 		header('Content-type: application/json');
 		echo $obj;
@@ -54,3 +54,50 @@
 	}
 	
 ?>
+
+<?php
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+$inData = json_decode(file_get_contents("php://input"), true);
+$conn = new mysqli("localhost", "Group4", "fouristhebest", "ContactMan");
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Database connection failed: " . $conn->connect_error]));
+}
+
+$login = $conn->real_escape_string($inData["login"]);
+$password = $inData["password"];
+
+$sql = "SELECT id, firstName, lastName, password FROM Users WHERE login=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $login);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($row = $result->fetch_assoc()) {
+    if (password_verify($password, $row["password"])) {
+        //store user session
+        $_SESSION["user_id"] = $row["id"];
+        $_SESSION["first_name"] = $row["firstName"];
+        $_SESSION["last_name"] = $row["lastName"];
+
+        echo json_encode([
+            "id" => $row["id"],
+            "firstName" => $row["firstName"],
+            "lastName" => $row["lastName"],
+            "message" => "Login successful"
+        ]);
+    } else {
+        echo json_encode(["error" => "Invalid password"]);
+    }
+} else {
+    echo json_encode(["error" => "User not found"]);
+}
+
+$conn->close();
+?>
+
