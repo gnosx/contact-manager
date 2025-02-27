8232de4d6954de4b338af7d94e16ce49d8752dbd
gnosx <qexaura21@gmail.com>
2025-02-27 01:01:58 -0500

    test

--- script.js
+++ script.js
@@ -134,7 +134,9 @@ function saveCookie()
 	let minutes = 20;
 	let date = new Date();
 	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + "; expires=" + date.toGMTString();
+    document.cookie = "lastName=" + lastName + "; expires=" + date.toGMTString();
+    document.cookie = "userId=" + userId + "; expires=" + date.toGMTString();
 }
 
 function readCookie()
