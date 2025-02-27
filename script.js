94fdb0a4b0cee376b18d8bd5d26c74ec6d12c045
Val <aviles.va05@gmail.com>
2025-02-26 21:27:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ let userId = 0;
 let firstName = "";
 let lastName = "";
 
-function doLogin()
-{
+function doLogin() {
 	userId = 0;
 	firstName = "";
 	lastName = "";
 
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
-	
+	//	var hash = md5( password );
+
 	document.getElementById("loginResult").innerHTML = "";
 
-	let tmp = {login:login,password:password};
-//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify( tmp );
-	
+	let tmp = { login: login, password: password };
+	//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify(tmp);
+
 	let url = urlBase + '/Login.' + extension;
 
 	fetch(url, {
@@ -28,36 +27,35 @@ function doLogin()
 		body: jsonPayload,
 		headers: { "Content-Type": "application/json" }
 	})
-	// request err
-	.then(response => {
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-		return response.json();
-	})
-	// success
-	.then(data => {
-		if (data.error === "") {
-			firstName = data.firstName;
-			lastName = data.lastName;
-			userId = data.userId;
-			saveCookie();
-			window.location.href = "contacts.html";
-		} else {
-			alert("Error: " + data.error);
-		}
-	})
-	// generic error
-	.catch(error => {
-		console.error("Error:", error);
-		alert("Login failed: " + error.message); // Show user feedback
-	});
+		// request err
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		// success
+		.then(data => {
+			if (data.error === "") {
+				firstName = data.firstName;
+				lastName = data.lastName;
+				userId = data.userId;
+				saveCookie();
+				window.location.href = "contacts.html";
+			} else {
+				alert("Error: " + data.error);
+			}
+		})
+		// generic error
+		.catch(error => {
+			console.error("Error:", error);
+			alert("Login failed: " + error.message); // Show user feedback
+		});
 }
 
-function doSignup()
-{
+function doSignup() {
 	userId = 0;
-	
+
 	let firstName = document.getElementById("firstName").value;
 	let lastName = document.getElementById("lastName").value;
 	let userName = document.getElementById("userName").value;
@@ -66,7 +64,7 @@ function doSignup()
 	// Include all fields to match PHP expectations
 	let tmp = { firstName: firstName, lastName: lastName, login: userName, password: userPass };
 	let jsonPayload = JSON.stringify(tmp);
-	
+
 	let url = urlBase + '/Create.' + extension;
 
 	fetch(url, {
@@ -74,58 +72,52 @@ function doSignup()
 		body: jsonPayload,
 		headers: { "Content-Type": "application/json" }
 	})
-	.then(response => {
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-		return response.json();
-	})
-	.then(data => {
-		if (data.error === "") {
-			alert("Account Created!");
-			window.location.href = "index.html";
-		} else {
-			alert("Error: " + data.error);
-		}
-	})
-	.catch(error => {
-		console.error("Error:", error);
-		alert("Sign up failed: " + error.message);
-	});
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			return response.json();
+		})
+		.then(data => {
+			if (data.error === "") {
+				alert("Account Created!");
+				window.location.href = "index.html";
+			} else {
+				alert("Error: " + data.error);
+			}
+		})
+		.catch(error => {
+			console.error("Error:", error);
+			alert("Sign up failed: " + error.message);
+		});
 }
 
-function saveCookie()
-{
+function saveCookie() {
 	let minutes = 20;
 	let date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
+	date.setTime(date.getTime() + (minutes * 60 * 1000));
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
-function readCookie()
-{
+function readCookie() {
 	userId = -1;
 	let data = document.cookie;
 	let splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
+	for (var i = 0; i < splits.length; i++) {
 		let thisOne = splits[i].trim();
 		let tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
+		if (tokens[0] == "firstName") {
 			firstName = tokens[1];
 		}
-		else if( tokens[0] == "lastName" )
-		{
+		else if (tokens[0] == "lastName") {
 			lastName = tokens[1];
 		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
+		else if (tokens[0] == "userId") {
+			userId = parseInt(tokens[1].trim());
 		}
 	}
-	
-	
-		document.getElementById("userName").innerHTML = "Logged in as " + userID + " " + lastName;
+
+
+	//document.getElementById("userName").innerHTML = "Logged in as " + userID + " " + lastName;
 
 }
