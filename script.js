309e5ba9118de13a4a8dc78eac6473ca2149962b
gnosx <qexaura21@gmail.com>
2025-02-28 21:46:38 -0500

    cookie test 2

--- script.js
+++ script.js
@@ -112,15 +112,16 @@ function saveCookie() {
 	let minutes = 20;
 	let date = new Date();
 	date.setTime(date.getTime() + (minutes * 60 * 1000));
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ";lastName=" + lastName + ";userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 function readCookie() {
 	userId = -1;
 	let data = document.cookie;
-	let splits = data.split(",");
+	let splits = data.split(";");
 	for (var i = 0; i < splits.length; i++) {
 		let thisOne = splits[i].trim();
+		console.log(thisOne);
 		let tokens = thisOne.split("=");
 		if (tokens[0] == "firstName") {
 			firstName = tokens[1];
@@ -129,6 +130,8 @@ function readCookie() {
 			lastName = tokens[1];
 		}
 		else if (tokens[0] == "userId") {
+			console.log(parseInt(tokens[1].trim()));
+			console.log(tokens[1].trim());
 			userId = parseInt(tokens[1].trim());
 		}
 	}
